ac702c6eab5fe0cade79153caf45c05c2d0077f2
PPYAPgit <ppyapmail@gmail.com>
2020-02-13 23:21:55 +0900

    dontbesad :D

--- satisfaction/satisfaction/urls.py
+++ satisfaction/satisfaction/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('praises.urls')),
+    path('', lambda request: redirect('dontbesad/', permanent=False)),
+    path('dontbesad/admin/', admin.site.urls),
+    path('dontbesad/', include('praises.urls')),
     path('accounts/', include('allauth.urls')),
 ]
 
